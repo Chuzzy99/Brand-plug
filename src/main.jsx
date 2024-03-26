a11847328724baf61cdce197652d6d88d820d85d
jubelo1234 <116165926+jubelo1234@users.noreply.github.com>
2024-03-26 16:54:29 +0100

    setup of routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+import Error from './pages/Error.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Blogs from './pages/Blogs.jsx';
+import Contact from './pages/Contact.jsx';
+import Projects from './pages/Projects.jsx';
+import Services from './pages/Services.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error/>,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/blogs", element: <Blogs /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/services", element: <Services /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
